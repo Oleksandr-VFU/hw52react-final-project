06d6a98d00e21e4ee6bf3026796a5cf144804dd3
Oleksandr Komnatskyi <akomnatskyi@gmail.com>
2025-08-26 21:59:37 +0300

    Feat: add reset filters functionality to restore default values

--- src/components/pages/Products.tsx
+++ src/components/pages/Products.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
+import { MdRefresh } from "react-icons/md"
 import useFetch from "../../hooks/useFetch"
 import { createUrl } from "../../utils/mockapi"
 import { API_ITEMS_PER_PAGE_LIMIT } from "../../utils/mockapi"
@@ -14,8 +15,18 @@ const Products = () => {
   const [name, setName] = useState('')
   const [sort, setSort] = useState('')
   const [order, setOrder] = useState('asc')
-    const {data: cars, isLoading, error} = useFetch<ProductInterface>(createUrl(page, name, sort, order), undefined, reload)
-    const debouncedSetName = debounce(setName, 1000)
+  const {data: cars, isLoading, error} = useFetch<ProductInterface>(createUrl(page, name, sort, order), undefined, reload)
+  const debouncedSetName = debounce(setName, 1000)
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const resetFilters = () => {
+    setName('')
+    setSort('')
+    setOrder('asc')
+    setPage(1)
+    inputRef.current && (inputRef.current.value = '')
+  }
 
   return (
     <div>
@@ -23,7 +34,7 @@ const Products = () => {
       <div className="products-filter">
         <div className="form-group">
           <label htmlFor="filter">Фільтр за назвою</label>
-          <input id="filter" className="products-filter__input" type="text" placeholder="Фільтрувати за назвою..." onChange={(e) => debouncedSetName(e.target.value)}/>
+          <input ref={inputRef} id="filter" className="products-filter__input" type="text" placeholder="Фільтрувати за назвою..." onChange={(e) => debouncedSetName(e.target.value)}/>
         </div>
         <div className="form-group">
           <label htmlFor="sort">Сортувати за</label>
@@ -45,6 +56,7 @@ const Products = () => {
             ))}
           </select>
         </div>
+        <button className="products-filter__reset" onClick={resetFilters}><MdRefresh /></button>
       </div>
       {isLoading && <h2 className="loading">Loading...</h2>}
       {error && <h2 className="error">{error}</h2>}
